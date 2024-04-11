6b8e04efff2ab5e1035a3153055cd8bd1349206c
bfloyd14 <brendanflo1425@gmail.com>
2024-04-11 16:05:51 -0400

    set up route and database_Url

---
+++ data/skill-data.js
@@ -0,0 +1,9 @@
+const skills = [
+  {text: 'Super Human Strength', done: true, _id: 125223},
+  {text: 'Good Looking', done: false, _id: 127904},
+  {text: 'Compassionate', done: false, _id: 139608},
+]
+
+export { 
+	skills
+}
